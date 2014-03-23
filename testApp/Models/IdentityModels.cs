7141b74dceea722c0cc8e79879eaf7c7714cc82f
test22 <mtr2022@hotmail.com>
2014-03-23 15:49:33 -0400

    models, controllers and connection string added

--- testApp/Models/IdentityModels.cs
+++ testApp/Models/IdentityModels.cs
@@ -5,12 +5,13 @@ namespace testApp.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection")
+            : base("db73668b188e6648bfaab6a2f701222f74Entities")
         {
         }
     }
